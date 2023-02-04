74245ef8c0c6aa6f56412a43df7ec8e10a5f3b43
Daniel Cornejo <contact@danielcornejo.dev>
2023-02-04 11:06:11 -0500

    tutorial # 11 complete

--- sandbox.ts
+++ sandbox.ts
@@ -162,3 +162,85 @@ const minus = (a: number, b: number): number => {
 //inferred type number
 let result = minus(10,7);
 console.log(result);
+
+//Aliases
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StrigOrNum };
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+}
+
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+}
+
+const greetAgain = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+}
+
+// let greet: Function;
+
+// example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+//alternatively you can define an alias and plug it into your function parameter TS below. 
+type person = {name: string, age: number};
+
+logDetails = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
+
+
+//TypeScript Tutorial #11 - The DOM & Type Casting
+//TSC knows that HTML element is an HTMLAnchorElement because is seleted by tag.
+const anchor = document.querySelector('a')!;
+if(anchor) {
+  console.log(anchor.href);
+}
+console.log(anchor.href);
+
+//If selected by class, TSC will not recognize what type of HTML element it is so you have to define it manually. 
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children); 
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+//prevent default prevents the page from refreshing upon submission, which is the default behavior.
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
